30e624ccb544a9e4a41cfa9b23b49bab818fd6fb
Mike Eling <mike.eling97@gmail.com>
2018-12-13 22:16:30 +0100

    🛠 Improved summon process

--- src/creators/react/index.js
+++ src/creators/react/index.js
@@ -1,7 +1,6 @@
-const exec = require("child_process").exec,
-  fs = require("fs-extra"),
 #!/usr/bin/env node
 
+const fs = require("fs-extra"),
   utils = require("../../utils.js");
 
 module.exports = function(path, callback) {
@@ -12,32 +11,23 @@ module.exports = function(path, callback) {
     path + "src/utils"
   ];
 
-  let execCallback = (error, stdout, stderr) => {
-    utils.clear();
+  utils.clear();
 
-    if (error) callback(error);
-    if (stderr) callback(stderr);
+   utils.log(`Installed create-react-app.`, "success");
+    utils.break();
 
-    if (stdout) {
-      utils.log(`Installed create-react-app.`, "success");
-      utils.break();
-
-      for (var i = 0; i < dirsStructure.length; i++) {
-        fs.mkdirsSync(dirsStructure[i]);
-        utils.log(
-          `Created ${dirsStructure[i].split(path)[1]} directory.`,
-          "progress"
-        );
-      }
-
-      fs.moveSync(`${path}/src/App.js`, `${path}/src/core/App.js`);
-      fs.moveSync(`${path}/src/App.test.js`, `${path}/src/core/App.test.js`);
-      fs.moveSync(`${path}/src/App.css`, `${path}/src/core/App.css`);
-      fs.moveSync(`${path}/src/logo.svg`, `${path}/src/core/logo.svg`);
-
-      callback();
+    for (var i = 0; i < dirsStructure.length; i++) {
+      fs.mkdirsSync(dirsStructure[i]);
+      utils.log(
+        `Created ${dirsStructure[i].split(path)[1]} directory.`,
+        "progress"
+      );
     }
-  };
 
-  exec(`npx create-react-app ${path}`, execCallback);
+    fs.moveSync(`${path}/src/App.js`, `${path}/src/core/App.js`);
+    fs.moveSync(`${path}/src/App.test.js`, `${path}/src/core/App.test.js`);
+    fs.moveSync(`${path}/src/App.css`, `${path}/src/core/App.css`);
+    fs.moveSync(`${path}/src/logo.svg`, `${path}/src/core/logo.svg`);
+
+    callback();
 };
--- src/index.js
+++ src/index.js
@@ -3,26 +3,45 @@
 
 const program = require("commander"),
   pkg = require("../package.json"),
+  chalk = require("chalk"),
   utils = require("./utils.js"),
+  exec = require("child_process").exec,
   fs = require("fs-extra"),
+  validate = require("validate-npm-package-name"),
   creators = require("./creators/index.js");
+
+/** CONSTANTS */
+
+const LIST = {
+  "react": {
+    package: "create-react-app",
+    setup: creators.createReactApp
+  }
+};
+
+/** ========== SETUP ========== */
+
+program
+  .version(pkg.version, "-v, --version");
 
 /** ========== COMMANDS ========== */
 
 program
-  .version(pkg.version, "-v, --version")
   .command("install <env> [path]")
   .description("Summon a ready-to-go web app structure.")
   .alias("i")
   .action(function(env, path) {
     if (!path) path = "./";
 
-
+    if(!LIST[env]) {
+      utils.log("This environment is not available!", "error");
+      return;
+    }
       
           utils.clear();
         
       
-          summon(env, path, err => {
+          summon(LIST[env], path, err => {
             if (err) utils.log(err, "error");
             utils.break();
             utils.log("Environment summoned!", "success");
@@ -30,11 +49,14 @@ program
   });
 
 program
-  .version(pkg.version, "-v, --version")
   .command("list")
   .alias("ls")
   .action(function() {
-    utils.log("For now it's just 'react' that you can summon!");
+    utils.log("List of available presets:");
+    utils.break();
+    for(var pckg in LIST) {
+      console.log(`- ${chalk.bold(pckg)} (${LIST[pckg].package})`);
+    }
   });
 
 program.parse(process.argv);
@@ -44,21 +66,26 @@ program.parse(process.argv);
 function summon(env, path, callback) {
   if(path[path.length-1] === "/") path = path.substring(0, path.length - 1);
 
-  if (utils.directoryExists(path)) {
-    utils.log(`The directory under "${path}" already exists!`, "error");
+  if(!validate(env.package)["validForNewPackages"]) {
+    callback(`Package ${env.package} is not valid.`);
     return;
   }
 
-  switch (env) {
-    case "react":
-      utils.log("Installing create-react-app...");
-      creators.createReactApp(path, err => {
-        callback(err);
-      });
-      return;
+  fs.ensureDir(path, err => {
+    if(err) { callback(err); return; }
+  
+    utils.log(`Installing ${env.package}...`);
 
-    default:
-      utils.log("There is no such environment!", "warning");
-      return;
-  }
+    exec(`npx create-react-app ${path}`, (error, stdout, stderr) => { 
+      if (error) { callback(error); return; }
+      if (stderr) { callback(stderr); return; }
+
+      if(stdout) {
+        env.setup(path, err => {
+          if(err) {callback(err); return;}
+          callback();
+        });
+      }
+    });
+  });
 }
